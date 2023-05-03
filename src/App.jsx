fc684d015a247da6d850b9440f2b6b88c25381de
reinaldoper <reinaldoper83@gmail.com>
2023-05-03 18:23:51 -0400

    feat: retira logos

--- src/App.jsx
+++ src/App.jsx
@@ -40,12 +40,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        {/* <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
       <h1>Vite + React</h1>
       <div className="card">
